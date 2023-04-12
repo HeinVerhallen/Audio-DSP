f44794770a9618e23703a2965ea8748d56686a4a
Hein Verhallen <92261377+HeinVerhallen@users.noreply.github.com>
2023-04-12 14:04:29 +0200

    update UI

--- Firmware/UI arduino/src/main.cpp
+++ Firmware/UI arduino/src/main.cpp
@@ -2,7 +2,7 @@
 #include "button.h"
 #include "Nextion.h"
 
-#define white "WHITE"
+#define empty "WHITE"
 #define black "BLACK"
 #define blue "BLUE"
 #define gray "50712"
@@ -38,42 +38,47 @@ Button* S3 = new Button(4);
 
 const char* channels[6] = {"Channel 1", "Channel 2","Channel 3", "Channel 4","Channel 5", "Channel 6"};
 
-const char* objects[10][6] = { 																//array for all clickable objects on the active page.
-	{"b0","b1","b2"}, 									//page 0 homeScreen
-	{"b0","b1","b2"}, 									//page 1 signalRouting
-	{"b0","t2","b1","b2", "b3"}, 				//page 2 effects
-	{"b0",}, 														//page 3 presets
-	{"b0",}, 														//page 4 channelRouting
+const char* objects[10][6] = {		//array for all clickable objects on the active page.
+	{"b0","b1","b2"}, 			//page 0 homeScreen
+	{"b0","b1","b2"}, 			//page 1 signalRouting
+	{"b0","t1","b1","b2", "b3"}, 		//page 2 effects
+	{"b0",}, 					//page 3 presets
+	{"b0",}, 					//page 4 channelRouting
 	{"b0","b1","b2","b3","b4","b5",}, 	//page 5 sourceSelect
-	{"b0",}, 														//page 6 linkChannels
-	{"b0",}, 														//page 7 gain
-	{"b0",}, 														//page 8 effectLoop
-	{"b0",}, 														//page 9 equalizer
+	{"b0",}, 					//page 6 linkChannels
+	{"b0",}, 					//page 7 gain
+	{"b0",}, 					//page 8 effectLoop
+	{"b0",}, 					//page 9 equalizer
 };
 
 	const int functions[10][7] = {
-		{signalRouting,effects,presets,0,0,0,3},				//page 0 homeScreen
-		{homeScreen,sourceSelect,channelRouting,0,0,0,3},			//page 1 signalRouting
+		{signalRouting,effects,presets,0,0,0,3},			//page 0 homeScreen
+		{homeScreen,sourceSelect,channelRouting,0,0,0,3},	//page 1 signalRouting
 		{homeScreen,dropdown,gain,effectLoop,equalizer,0,5},	//page 2 effects
-		{homeScreen,0,0,0,0,0,1},															//page 3 presets
-		{signalRouting,0,0,0,0,0,1},															//page 4 channelRouting
-		{signalRouting,RCA1,TRS1,RCA2,TRS2,save,6,},							//page 5 sourceSelect
-		{channelRouting,0,0,0,0,0,1},															//page 6 linkChannels
-		{effects,0,0,0,0,0,1},															//page 7 gain
-		{effects,0,0,0,0,0,1},															//page 8 effectLoop
-		{effects,0,0,0,0,0,1},															//page 9 equalizer
+		{homeScreen,0,0,0,0,0,1},					//page 3 presets
+		{signalRouting,0,0,0,0,0,1},					//page 4 channelRouting
+		{signalRouting,RCA1,TRS1,RCA2,TRS2,save,6,},		//page 5 sourceSelect
+		{channelRouting,0,0,0,0,0,1},					//page 6 linkChannels
+		{effects,0,0,0,0,0,1},						//page 7 gain
+		{effects,0,0,0,0,0,1},						//page 8 effectLoop
+		{effects,0,0,0,0,0,1},						//page 9 equalizer
 	};
 
-	int16_t state = 0;
-	int16_t currentPage = 0;
-	int16_t highlite = 0;
-	int16_t lastHighlite = 0;
-	int16_t navMenu = 1;
+	const int dropdown1[] = {
+		
+	};
+
+	int8_t state = 0;
+	int8_t currentPage = 0;
+	int8_t highlight = 0;
+	int8_t lasthighlight = 0;
+	int8_t navMenu = 1;
+	int8_t activeChannel = 0;
 
 void update(){
-	changeParameter(objects[currentPage][lastHighlite], gray, backcolor);
-	changeParameter(objects[currentPage][highlite], blue, backcolor);
-	lastHighlite = highlite;
+	changeParameter(objects[currentPage][lasthighlight], gray, backcolor);
+	changeParameter(objects[currentPage][highlight], blue, backcolor);
+	lasthighlight = highlight;
 }
 
 void setup() {
@@ -85,7 +90,7 @@ void setup() {
 
 void updatePage(int newPage){
 	currentPage = newPage;
-	highlite = 0;
+	highlight = 0;
 	gotoPage(currentPage);
 	update();
 }
@@ -95,58 +100,60 @@ void updatePage(int newPage){
 //        active – the current active option as an integer index
 //Outputs: -
 void channelSelect(int16_t toggle, int16_t active){
-  static int previous = 0;
+  	static int previous = 0;
+	static int on = 0;
 
-  if(toggle){                                                     //activate or update the dropdown list
-    for(uint16_t i = 0; i<sizeof(channels)/sizeof(const char*); i++){  //loop through every option in the dropdown list
-      textField(i+2, channels[i], text);                        //show all options on the screen
-    }
-    textField(previous+2, white, backcolor);                        //dehighlite the previous selected option
-    textField(active+2, blue, backcolor);                           //highlite the new selected option
-  } else{                                                         //deactivate the dropdown list
-    for(uint16_t i = 0; i<sizeof(channels)/sizeof(const char*); i++){  //loop through every option in the dropdown list
-      if(i==0)
-        textField(i+2, channels[active], text);                 //show the active option on the screen
-      else
-        textField(i+2, "", text);                               //make the rest empty
-
-      textField(i+2, white, backcolor);                             //set all background colors to neutral
-    }
-  }
-  previous = active;                                              //upate the previous active option
+  	if(toggle){
+		on=!on;
+		if(on){											//activate or update the dropdown list
+    			for(uint16_t i = 1; i<sizeof(channels)/sizeof(const char*)+1; i++){ //loop through every option in the dropdown list
+      			textField(i, channels[i], text);					//show all options on the screen
+    			}
+		}else{
+			for(uint16_t i = 1; i<sizeof(channels)/sizeof(const char*)+1; i++){
+				if(i==1)
+					changeParameter(objects[2][i],channels[activeChannel],0);
+				else
+					textField(i,"",0);
+			}
+			textField(previous, empty, 1);
+		}
+  	} else{											//deactivate the dropdown list
+    		textField(previous+1, empty, backcolor);					//dehighlight the previous selected option
+    		textField(active+1, blue, backcolor);					//highlight the new selected option
+  	}
+  	previous = active;                                              		//upate the previous active option
 }
 
 void loop() {
-	static int16_t activeOption = 0;
-  if(S1->debounce()){
-		int function = functions[currentPage][highlite];//get the function associated with the selected object
-		if(function < back){														//go to the selected page
+	if(S1->debounce()){
+		int function = functions[currentPage][highlight];	//get the function associated with the selected object
+		if(function < back){						//go to the selected page
 			updatePage(function);
-		}else if(function == dropdown){	//Something was selected
-		static int state = 0;
-		state = !state;
-			channelSelect(state,0);
+		}else if(function == dropdown){				//Something was selected
+			navMenu = !navMenu;
+			channelSelect(!navMenu,0);
 		}
-  }
-  if(S2->debounce() == 1){
+  	}
+  	if(S2->debounce() == 1){
 		if(navMenu){
-			highlite= (highlite + 1) % (functions[currentPage][6]);
+			highlight= (highlight + 1) % (functions[currentPage][6]);
 			update();
 		}else{
-			activeOption = (activeOption+1)%(sizeof(channels)/sizeof(channels[0]));
-			channelSelect(state, activeOption);
+			activeChannel = (activeChannel+1)%(sizeof(channels)/sizeof(channels[0]));
+			channelSelect(navMenu, activeChannel);
 		}
 	}
-  if(S3->debounce()){
+  	if(S3->debounce()){
 		if(navMenu){
-			highlite = (highlite == 0) ? functions[currentPage][6]-1: highlite-1;
+			highlight = (highlight == 0) ? functions[currentPage][6]-1: highlight-1;
 			update();
 		}else {
-			activeOption = (activeOption = 0) ? sizeof(channels)/sizeof(channels[0])-1 : activeOption-1;
-			channelSelect(state, activeOption);
+			activeChannel = (activeChannel = 1) ? sizeof(channels)/sizeof(channels[0])-1 : activeChannel-1;
+			channelSelect(navMenu, activeChannel);
 		}
-  }
-}
+  	}
+}//*/
 
 
 //Gather all information from the DSP controller

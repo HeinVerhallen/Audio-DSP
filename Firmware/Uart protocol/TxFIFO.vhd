bf7f4fda428bf1aecca7ead7afe8e0f8064076a5
Hein Verhallen <92261377+HeinVerhallen@users.noreply.github.com>
2023-06-07 09:43:22 +0200

    Updated UART protocol
    finished simulating the system
    the hardware test is yet to be performed

--- Firmware/Uart protocol/TxFIFO.vhd
+++ Firmware/Uart protocol/TxFIFO.vhd
@@ -6,7 +6,7 @@ PORT(
 	Data			: in	std_logic_vector(7 downto 0);
 	Load			: in	std_logic;
 	Ready			: in	std_logic;
-	BufferFull	: inout	std_logic := '0';
+	BufferFull	: out	std_logic := '0';
 	dataReady	: out	std_logic;
 	Byte			: out	std_logic_vector(7 downto 0)
 );
@@ -18,22 +18,25 @@ TYPE memory IS ARRAY(15 DOWNTO 0) OF std_logic_vector(7 DOWNTO 0);
 signal FIFO 	: memory;
 BEGIN
 PROCESS ( Load, Ready )
-variable head	: integer := 0;
-variable tail	: integer := 0;
+VARIABLE bufFull : std_logic := '0'; 
+
+VARIABLE head	: integer := 0;
+VARIABLE tail	: integer := 0;
 BEGIN
 	IF ready = '1' THEN
 		Byte <= FIFO(tail);
 		Dataready <= '1';
 		tail := (tail + 1) mod 16;
 		
-		IF BufferFull = '1' THEN
+		IF bufFull = '1' THEN
+			bufFull := '0';
 			BufferFull <= '0';
 		END IF;
 		
 	ELSIF Load = '1' THEN
 		Dataready <= '0';
 		
-		IF BufferFull = '0' THEN
+		IF bufFull = '0' THEN
 			--insert incoming byte
 			FIFO(head) <= Data;
 			
@@ -42,8 +45,10 @@ BEGIN
 			
 			--full condition (if after byte is loaded the head = tail then the buffer must be full)
 			IF head = tail THEN
+				bufFull := '1';
 				BufferFull <= '1';
 			ELSE
+				bufFull := '0';
 				BufferFull <= '0';
 			END IF;
 		END IF;

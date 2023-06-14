d471d3e328275fafcc6f4acde3c8f34e424d52eb
busene <busse@beechleaf.nl>
2023-06-14 11:22:31 +0200

    updated and simulink

--- Firmware/Effects Matlab/reverb.m
+++ Firmware/Effects Matlab/reverb.m
@@ -3,10 +3,10 @@ clear all;
 close all;
 
 
-[y,Fs] = audioread('pen15.wav');    % Read WAV file
+[y,Fs] = audioread('muted.wav');    % Read WAV file
 %soundsc(y,Fs);                        % Play WAV file
 
-factor = 0.3;
+factor = 0.15;
 
 delay1 = round(Fs*factor^1);  
 delay2 = round(Fs*factor^2);  
@@ -20,4 +20,4 @@ yy2 = filter([1 zeros(1,delay3) coef],[1 zeros(1,delay4) -coef],y);
 yytotal = yy1+yy2;
 
 
-soundsc(yytotal,Fs);
+soundsc(yy1,Fs);

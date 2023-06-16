32af6534473d53c819a5a8c8b0dd0d252cde59dc
Youri <youritils@live.nl>
2023-06-17 00:04:44 +0200

    Update multiplier_rounder.vhd
    Fixed bug where small value results would also be shifted to the MSB, resulting in a wrong answer. Fixed this problem for unsigned, signed and STD_LOGIC_VECTOR types. Verified that it works with the do file.

--- Firmware/mult_rounder.do
+++ Firmware/mult_rounder.do
@@ -1,11 +1,17 @@
 restart -f
 
-force a 1101
-force b 0010
+force a 00000101
+force b 00001010
 run 20
 
-force a 1100
+force a 01100110
+force b 10101010
 run 20
 
-force b 0011
+force a 11111111
+force b 11111111
+run 20
+
+force a 11110011
+force b 00000001
 run 20
--- Firmware/multiplier_rounder.vhd
+++ Firmware/multiplier_rounder.vhd
@@ -6,9 +6,10 @@ entity multiplier_rounder is
     GENERIC(
         d_width : integer := 8);
     Port ( 
-        a       : in  std_logic_vector(d_width-1 downto 0); 
-        b       : in  std_logic_vector(d_width-1 downto 0);                          
-        output  : out std_logic_vector(d_width-1 downto 0)
+        a               : in  std_logic_vector(d_width-1 downto 0); 
+        b               : in  std_logic_vector(d_width-1 downto 0);                          
+        unsigned_output : out std_logic_vector(d_width-1 downto 0);
+        signed_output   : out std_logic_vector(d_width-1 downto 0)
         ); 
 end multiplier_rounder;
 
@@ -22,15 +23,16 @@ architecture Behavioral of multiplier_rounder is
         variable temp_b         : unsigned(b'length-1 downto 0);
         variable temp           : unsigned(a'length*2-1 downto 0);
         variable temp_mirror    : unsigned(0 to a'length*2-1);
-        variable shifter        : unsigned(a'length*2-1 downto 0);
-        variable invert        : unsigned(a'length*2-1 downto 0);
-        variable andGate        : unsigned(a'length*2-1 downto 0);
+        variable max            : unsigned(a'length-1 downto 0) := (others => '1');
+        --variable shifter        : unsigned(a'length*2-1 downto 0);
+        --variable invert         : unsigned(a'length*2-1 downto 0);
+        --variable andGate        : unsigned(a'length*2-1 downto 0);
     begin
         temp_a := unsigned(a);
         temp_b := unsigned(b);
 
-        report "temp_a: " & integer'image(to_integer(temp_a));
-        report "temp_b: " & integer'image(to_integer(temp_b));
+        --report "temp_a: " & integer'image(to_integer(temp_a));
+        --report "temp_b: " & integer'image(to_integer(temp_b));
 
         if (temp_a(a'left) = '1') then  --if signed
             temp_a := (not temp_a) + "1";   --make unsigned
@@ -40,39 +42,46 @@ architecture Behavioral of multiplier_rounder is
             temp_b := (not temp_b) + "1";   --make unsigned
         end if;
 
-        report "temp_a: " & integer'image(to_integer(temp_a));
-        report "temp_b: " & integer'image(to_integer(temp_b));
+        --report "temp_a: " & integer'image(to_integer(temp_a));
+        --report "temp_b: " & integer'image(to_integer(temp_b));
 
-        temp := unsigned(temp_a * temp_b);
-        report "temp: " & integer'image(to_integer(temp));
+        temp := temp_a * temp_b;
+        --report "temp: " & integer'image(to_integer(temp));
 
         for i in 0 to a'length*2-1 loop
             temp_mirror(i) := temp(i);
         end loop;
 
-        report "temp_mirror: " & integer'image(to_integer(temp_mirror));
+        --report "temp_mirror: " & integer'image(to_integer(temp_mirror));
 
-        report "lsb of a: " & boolean'image(a(a'left)='1');
+        --report "lsb of a: " & boolean'image(a(a'left)='1');
 
         if (a(a'left) = '1' xor b(b'left) = '1') then   --if result is negative
-            report "was true";
+            --report "was true";
             temp := (not temp) + "1";           --make signed
         end if;
 
-        report "temp: " & integer'image(to_integer(temp));
+        --report "temp: " & integer'image(to_integer(temp));
 
-        invert := not (unsigned(temp_mirror) - "1");
-        report "invert: " & integer'image(to_integer(invert));
-        andGate := unsigned(temp_mirror) and invert;
-        report "andGate: " & integer'image(to_integer(andGate));
-        shifter := (unsigned(temp_mirror) and (not (unsigned(temp_mirror) - "1"))) / 2;
+        --invert := not (unsigned(temp_mirror) - "1");
+        --report "invert: " & integer'image(to_integer(invert));
+        --andGate := unsigned(temp_mirror) and invert;
+        --report "andGate: " & integer'image(to_integer(andGate));
+        --shifter := (unsigned(temp_mirror) and (not (unsigned(temp_mirror) - "1"))) / 2;
 
-        report "shifter: " & integer'image(to_integer(shifter));
+        --report "shifter: " & integer'image(to_integer(shifter));
 
-        result := signed(temp * shifter);
-        --result := signed(unsigned(temp) * ((unsigned(temp_mirror) and not (unsigned(temp_mirror) - "1")) / 2));
+        --result := signed(temp * shifter);
+        if ((temp(temp'left) = '0' and temp > max) or (temp(temp'left) = '1' and (not temp > max))) then
+            report "Is larger!";
+            result := signed(temp * ((temp_mirror and not (temp_mirror - "1")) / 2));
+        else
+            report "Is smaller!";
+            result(a'length*3-1 downto a'length) := signed(temp);
+            --result := ZERO(a'length-1 downto 0) & temp & ZERO(a'length-1 downto 0);
+        end if;
 
-        report "result: " & integer'image(to_integer(result));
+        report "result: " & integer'image(to_integer(result(a'length*2-1 downto a'length)));
         report "result signed: " & integer'image(to_integer(result(a'length*2-1 downto a'length)));
 
         return result(a'length*2-1 downto a'length);
@@ -85,14 +94,25 @@ architecture Behavioral of multiplier_rounder is
         variable result         : unsigned(a'length*4-1 downto 0);
         variable temp           : unsigned(a'length*2-1 downto 0);
         variable temp_mirror    : unsigned(0 to a'length*2-1);
+        variable max            : unsigned(a'length-1 downto 0) := (others => '1');
     begin
         temp := a * b;
+        report "temp: " & integer'image(to_integer(temp));
 
         for i in 0 to a'length*2-1 loop
             temp_mirror(i) := temp(i);
         end loop;
 
-        result := temp * (temp_mirror and not (temp_mirror - "1"));
+        if (temp > max) then
+            report "Is larger!";
+            result := temp * (temp_mirror and not (temp_mirror - "1"));
+        else
+            report "Is smaller!";
+            result(a'length*3-1 downto a'length) := temp;
+            --result := ZERO(a'length-1 downto 0) & temp & ZERO(a'length-1 downto 0);
+        end if;
+        
+        report "result: " & integer'image(to_integer(result(a'length*2-1 downto a'length)));
 
         return result(a'length*2-1 downto a'length);
     end multiply;
@@ -104,6 +124,7 @@ architecture Behavioral of multiplier_rounder is
         variable result         : std_logic_vector(a'length*4-1 downto 0);
         variable temp           : unsigned(a'length*2-1 downto 0);
         variable temp_mirror    : unsigned(0 to a'length*2-1);
+        variable max            : unsigned(a'length-1 downto 0) := (others => '1');
     begin
         temp := unsigned(a) * unsigned(b);
 
@@ -111,22 +132,30 @@ architecture Behavioral of multiplier_rounder is
             temp_mirror(i) := temp(i);
         end loop;
 
-        result := std_logic_vector(temp * (temp_mirror and not (temp_mirror - "1")));
+        if (temp > max) then
+            report "Is larger!!!";
+            result := std_logic_vector(temp * (temp_mirror and not (temp_mirror - "1")));
+        else
+            report "Is smaller!!!";
+            result(a'length*3-1 downto a'length) := std_logic_vector(temp);
+            --result := ZERO(a'length-1 downto 0) & temp & ZERO(a'length-1 downto 0);
+        end if;
 
         return result(a'length*2-1 downto a'length);
     end multiply;
 
-    signal c : unsigned(d_width-1 downto 0);-- := multiply("1010","0101"); -- dit werkt!
-    signal p : signed(d_width-1 downto 0) := to_signed(-12, d_width);
-    signal q : signed(d_width-1 downto 0) := to_signed(12, d_width);
-    signal s : signed(d_width-1 downto 0);
+    --signal c : unsigned(d_width-1 downto 0);-- := multiply("1010","0101"); -- dit werkt!
+    --signal p : signed(d_width-1 downto 0) := to_signed(-1, d_width);
+    --signal q : signed(d_width-1 downto 0) := to_signed(13, d_width);
+    --signal s : signed(d_width-1 downto 0);
 
 begin
     process(a, b)
     begin
-        c       <= multiply(unsigned(a),unsigned(b));
-        output  <= multiply(a,b);
-        s <= multiply(p,q);
+        --c       <= multiply(unsigned(a),unsigned(b));
+        unsigned_output  <= multiply(a,b);
+        --s <= multiply(p,q);
+        signed_output <= std_logic_vector(multiply(signed(a), signed(b)));
 
         --report "p: " & integer'image(to_integer(p));
     end process;
